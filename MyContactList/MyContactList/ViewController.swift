db347eda3d4df3f59509e4cee90cd4217fb31582
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2023-06-11 10:17:42 +0300

    master
    MyContactList: Initial Commit

---
+++ MyContactList/MyContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyContactList
+//
+//  Created by Ahmed Fayek on 11/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
